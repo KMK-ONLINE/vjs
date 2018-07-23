20e998f3e33bab215bb523c4eff0438afd9adbcd
Hendyanto <pair+hendyanto@kmkonline.co.id>
2018-07-23 17:48:54 +0700

    [#159162017] Added contrib dash to requirements, update version

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var videojs = require('video.js');
 window.videojs = videojs;
 require('videojs-flash');
+require('videojs-contrib-dash');
 require('videojs-contrib-hls');
 window.videojs.registerPlugin('ads', require('videojs-contrib-ads'));
 require('videojs-ima');
